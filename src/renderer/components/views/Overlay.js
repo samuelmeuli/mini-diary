6f7d9c3d23cca5f5bfc4bf2bd8df1a90bc434b20
Samuel Meuli <me@samuelmeuli.com>
2018-12-11 13:11:38 +0100

    Close preferences on esc key press

--- src/renderer/components/views/Overlay.js
+++ src/renderer/components/views/Overlay.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import iconClear from 'feather-icons/dist/icons/x.svg';
 import SimpleSvg from 'react-simple-svg';
@@ -12,24 +12,49 @@ const propTypes = {
 	onClose: PropTypes.func.isRequired
 };
 
-export default function Overlay(props) {
-	const { children, onClose } = props;
-	return (
-		<div className="overlay-outer">
-			<div className="overlay-inner">
-				<button
-					type="button"
-					className="button button-invisible overlay-close-button"
-					onClick={onClose}
-				>
-					<SimpleSvg src={iconClear} height={20} width={20} />
-				</button>
-				<div className="overlay-content">
-					{children}
+export default class Overlay extends PureComponent {
+	constructor() {
+		super();
+
+		// Function bindings
+		this.onKeyDown = this.onKeyDown.bind(this);
+	}
+
+	componentDidMount() {
+		window.addEventListener('keydown', this.onKeyDown);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('keydown', this.onKeyDown);
+	}
+
+	onKeyDown(e) {
+		if (e.key === 'Escape') {
+			const { onClose } = this.props;
+			onClose();
+		}
+	}
+
+	render() {
+		const { children, onClose } = this.props;
+
+		return (
+			<div className="overlay-outer">
+				<div className="overlay-inner">
+					<button
+						type="button"
+						className="button button-invisible overlay-close-button"
+						onClick={onClose}
+					>
+						<SimpleSvg src={iconClear} height={20} width={20} />
+					</button>
+					<div className="overlay-content">
+						{children}
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 Overlay.propTypes = propTypes;
