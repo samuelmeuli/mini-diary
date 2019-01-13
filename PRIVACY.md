c2b272e80bfcdace20f73ecc4517e6b0107aa335
Samuel Meuli <me@samuelmeuli.com>
2019-01-13 21:30:52 +0100

    Add privacy policy

---
+++ PRIVACY.md
@@ -0,0 +1,19 @@
+# Privacy Policy
+
+## Desktop Application
+
+The Mini Diary desktop application does not collect any personal information.
+
+
+## Website
+
+The [minidiary.app](https://minidiary.app) website uses [Fathom Analytics](https://usefathom.com).
+
+Fathom Analytics is website analytics software that tracks and reports aggregate website traffic without compromising on your privacy. Fathom only reports on aggregates, so no information specific to the website visitor is ever tracked or recorded.
+
+You can opt out of tracking by the Fathom software by enabling "Do Not Track" in your browser settings.
+
+
+## Contact
+
+Please do not hesitate to [contact me](https://samuelmeuli.com) for questions about this privacy policy.
--- src/main/menu/menus/help.js
+++ src/main/menu/menus/help.js
@@ -1,10 +1,30 @@
+const { shell } = require('electron');
+
+const URL_WEBSITE = 'https://minidiary.app';
+const URL_LICENSE = 'https://github.com/samuelmeuli/mini-diary/blob/master/LICENSE';
+const URL_PRIVACY_POLICY = 'https://github.com/samuelmeuli/mini-diary/blob/master/PRIVACY.md';
+
+
 module.exports = {
 	role: 'help',
 	submenu: [
 		{
-			label: 'Learn More',
+			label: 'Website',
 			click() {
-				require('electron').shell.openExternal('https://minidiary.app');
+				shell.openExternal(URL_WEBSITE);
+			}
+		},
+		{ type: 'separator' },
+		{
+			label: 'License',
+			click() {
+				shell.openExternal(URL_LICENSE);
+			}
+		},
+		{
+			label: 'Privacy Policy',
+			click() {
+				shell.openExternal(URL_PRIVACY_POLICY);
 			}
 		}
 	]
