dd5ad8caaf8c666db8b7478a5e6cdb5f4f89c272
Samuel Meuli <me@samuelmeuli.com>
2019-01-22 18:00:50 +0100

    Convert license to Markdown

--- src/main/menu/menus/help.js
+++ src/main/menu/menus/help.js
@@ -1,7 +1,7 @@
 const { shell } = require('electron');
 
 const URL_WEBSITE = 'https://minidiary.app';
-const URL_LICENSE = 'https://github.com/samuelmeuli/mini-diary/blob/master/LICENSE';
+const URL_LICENSE = 'https://github.com/samuelmeuli/mini-diary/blob/master/LICENSE.md';
 const URL_PRIVACY_POLICY = 'https://github.com/samuelmeuli/mini-diary/blob/master/PRIVACY.md';
 
 
