5f3afe8950d1f57b9412f777ae39725a59d77d71
jhernandis <jhernandis@gmail.com>
2019-01-29 14:51:18 +0100

    Added new translation file "es.js"

---
+++ src/main/i18n/translations/es.js
@@ -0,0 +1,92 @@
+// prettier-ignore
+module.exports = {
+	// Menu (defined by macOS)
+	'about-app': 'Acerca de {appName}',
+	'bring-all-to-front': 'Traer todo al frente',
+	'close': 'Cerrar',
+	'copy': 'Copiar',
+	'cut': 'Cortar',
+	'edit': 'Editar',
+	'file': 'Fichero',
+	'help': 'Ayuda',
+	'hide-app': 'Ocultar {appName}',
+	'hide-others': 'Ocultar otros',
+	'minimize': 'Minimizar',
+	'paste': 'Pegar',
+	'preferences': 'Preferencias',
+	'quit-app': 'Salir de {appName}',
+	'redo': 'Rehacer',
+	'select-all': 'Seleccionar todo',
+	'show-all': 'Mostrar todo',
+	'speech': 'Habla',
+	'start-speaking': 'Iniciar habla',
+	'stop-speaking': 'Detener habla',
+	'undo': 'Deshacer',
+	'view': 'Ver',
+	'window': 'Ventana',
+	'zoom': 'Ampliar',
+
+	// Menu (app-specific)
+	'export': 'Exportar',
+	'export-to-format': 'Exportar a {format}',
+	'import': 'Importar',
+	'import-from-format': 'Importar desde {format}',
+	'license': 'Licencia',
+	'lock-diary': 'Bloquear diario',
+	'next-day': 'Día siguiente',
+	'next-month': 'Mes siguiente',
+	'previous-day': 'Día anterior',
+	'previous-month': 'Mes anterior',
+	'privacy-policy': 'Política de privacidad',
+	'website': 'Página web',
+
+	// Theme
+	'dark': 'Oscuro',
+	'light': 'Claro',
+	'theme': 'Tema',
+
+	// Calendar
+	'today': 'Hoy',
+
+	// Editor
+	'add-a-title': 'Pon un título',
+	'write-something': 'Escribe alguna cosa',
+
+	// Search
+	'no-results': 'Sin resultados',
+	'no-title': 'Sin título',
+	'search': 'Buscar',
+
+	// Password and directory
+	'change-directory': 'Cambiar carpeta',
+	'change-password': 'Cambiar clave',
+	'diary-file': 'Fichero para el diario',
+	'move-error-msg': 'Ha ocurrido un error al mover el fichero',
+	'move-error-title': 'Error al mover fichero',
+	'move-file': 'Mover fichero',
+	'new-password': 'Nueva clave',
+	'password': 'Clave',
+	'passwords-no-match': 'Las claves no coinciden',
+	'repeat-new-password': 'Repite nueva clave',
+	'repeat-password': 'Repite clave',
+	'select-directory': 'Selecciona carpeta',
+	'set-password': 'Establecer clave',
+	'unlock': 'Desbloquear',
+
+	// Import
+	'import-backup-info': 'Mini Diary realizará una copia de seguridad de tus datos antes de la importación.',
+	'import-error-msg': 'Ha ocurrido un error durante la importación',
+	'import-error-title': 'Error importando',
+	'import-instructions-day-one': 'Abre la aplicaicón Day One y exporta tu diario en Fichero → Exportar → Texto plano. Descomprime el fichero guardado. Selecciona el TXT en el siguiente paso para importarlo a Mini Diary.',
+	'import-instructions-jrnl': 'Para exportar tu diario jrnl, ejecuta {command}. Selecciona el fichero JSON generado en el siguiente paso para importarlo a Mini Diary.',
+	'import-instructions-json': 'Puedes importar los datos de una exportación previa desde Mini Diary en formato JSON o desde otro fichero JSON que tenga el formato adecuado.',
+	'start-import': 'Iniciar importación',
+
+	// Export
+	'export-error-msg': 'Ha ocurrido un error durante la exportación',
+	'export-error-title': 'Error exportando',
+
+	// Other
+	'invalid-date': 'Fecha no válida',
+	'loading': 'Cargando'
+};
