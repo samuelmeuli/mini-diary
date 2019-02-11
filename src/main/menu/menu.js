4688739e8d3e4d50a025a3c018849904558fce6c
Samuel Meuli <me@samuelmeuli.com>
2019-02-11 17:39:30 +0100

    Enable lock screen preferences in MAS build

--- src/main/menu/menu.js
+++ src/main/menu/menu.js
@@ -1,6 +1,4 @@
 const { Menu } = require('electron');
-const is = require('electron-is');
-
 const { getMenuTemplate } = require('./template');
 
 const DISABLED_MENU_ITEMS = [
@@ -18,29 +16,24 @@ const DISABLED_MENU_ITEMS = [
 	'nextMonth'
 ];
 
-// Disable preferences in MAS build when the app is locked (no options that can be changed)
-if (is.mas()) {
-	DISABLED_MENU_ITEMS.push('preferences');
-}
-
 // Build menu
 const template = getMenuTemplate();
 const menu = Menu.buildFromTemplate(template);
 Menu.setApplicationMenu(menu);
 
-
-exports.disableMenuItems = () => {
-	DISABLED_MENU_ITEMS.forEach((id) => {
+function disableMenuItems() {
+	DISABLED_MENU_ITEMS.forEach(id => {
 		menu.getMenuItemById(id).enabled = false;
 	});
-};
+}
 
-
-exports.enableMenuItems = () => {
-	DISABLED_MENU_ITEMS.forEach((id) => {
+function enableMenuItems() {
+	DISABLED_MENU_ITEMS.forEach(id => {
 		menu.getMenuItemById(id).enabled = true;
 	});
+}
+
+module.exports = {
+	disableMenuItems,
+	enableMenuItems
 };
-
-
-exports.disableMenuItems();
