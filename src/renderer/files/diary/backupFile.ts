78a9818198e03cb5938819ed07a3d803265c7672
Samuel Meuli <me@samuelmeuli.com>
2019-04-02 23:38:39 +0200

    Fix backup file path for paths including dots

--- src/renderer/files/diary/backupFile.ts
+++ src/renderer/files/diary/backupFile.ts
@@ -2,12 +2,16 @@ import { toFilenameDate } from "../../utils/dateFormat";
 import { copyFile } from "../fileAccess";
 import { getDiaryFilePath } from "./diaryFile";
 
+const FILE_NAME_REGEX = /\.[^/.]+$/;
+
 /**
  * Create a copy of the (encrypted) diary file and append "-backup-[dateTime]" to its name
  */
 export function createBackup(): void {
 	const diaryFilePath = getDiaryFilePath();
 	const dateTime = toFilenameDate(new Date());
-	const backupPath = `${diaryFilePath.split(".")[0]}-backup-${dateTime}.txt`;
+
+	// Generate path to backup file: Remove file extension, append "-backup-DATE.txt"
+	const backupPath = `${diaryFilePath.replace(FILE_NAME_REGEX, "")}-backup-${dateTime}.txt`;
 	copyFile(diaryFilePath, backupPath);
 }
