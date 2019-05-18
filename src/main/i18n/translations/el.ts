42ebbf395c9c510eb6b844f39ab5c505b11ba7cf
Unknown <kdoumani98@gmail.com>
2019-05-19 01:36:45 +0300

    Add Greek translations

---
+++ src/main/i18n/translations/el.ts
@@ -0,0 +1,106 @@
+import { Translations } from "./types";
+
+// prettier-ignore
+const translationsEn: Translations = {
+	// Menu (defined by macOS)
+	"about-app": "Πληροφορίες για το {appName}",
+	"bring-all-to-front": "Επαναφορά όλων στο προσκήνιο",
+	"close": "Κλείσιμο",
+	"copy": "Αντιγραφή",
+	"cut": "Αποκοπή",
+	"edit": "Επεξεργασία",
+	"file": "Αρχείο",
+	"help": "Βοήθεια",
+	"hide-app": "Απόκρυψη {appName}",
+	"hide-others": "Απόκρυψη υπόλοιπων",
+	"minimize": "Ελαχιστοποίηση",
+	"paste": "Επικόλληση",
+	"preferences": "Προτιμήσεις",
+	"quit-app": "Έξοδος {appName}",
+	"redo": "Επανάληψη",
+	"select-all": "Επιλογή Όλων",
+	"show-all": "Εμφάνιση Όλων",
+	"speech": "Ομιλία",
+	"start-speaking": "Ξεκινήστε να μιλάτε",
+	"stop-speaking": "Σταματήστε να μιλάτε",
+	"undo": "Αναίρεση",
+	"view": "Προβολή",
+	"window": "Παράθυρο",
+	"zoom": "Μεγένθυση",
+
+	// Menu (app-specific)
+	"export": "Εξαγωγή",
+	"export-to-format": "Εξαγωγή σε {format}",
+	"import": "Εισαγωγή",
+	"import-from-format": "Εισαγωγή από {format}",
+	"license": "Άδειες",
+	"lock-diary": "Κλείδωμα Ημερολογίου",
+	"next-day": "Επόμενη Ημέρα",
+	"next-month": "Επόμενος Μήνας",
+	"previous-day": "Προηγούμενη Ημέρα",
+	"previous-month": "Προηγούμενος Μήνας",
+	"privacy-policy": "Πολιτική Απορρήτου",
+	"website": "Ιστοσελίδα",
+
+	// Theme
+	"auto": "Αυτόματο",
+	"dark": "Σκοτεινό",
+	"light": "Φωτεινό",
+	"theme": "Θέμα",
+
+	// Calendar
+	"today": "Σήμερα",
+
+	// Editor
+	"add-a-title": "Προσθήκη Τίτλου",
+	"bold": "Έντονα",
+	"bullets": "Κουκκίδες",
+	"italic": "Πλάγια",
+	"list": "Λίστα",
+	"write-something": "Γράψτε Κάτι",
+
+	// Search
+	"clear": "Καθαρισμός",
+	"no-results": "Δεν υπάρχουν αποτελέσματα",
+	"no-title": "Χωρίς Τίτλο",
+	"search": "Αναζήτηση",
+
+	// Password and directory
+	"change-directory": "Αλλαγή τοποθεσίας",
+	"change-password": "Αλλαγή κωδικού",
+	"choose-password": "Παρακαλώ επιλέξτε κωδικό για το ημερολόγιο.",
+	"decryption-error": "Σφάλμα αποκρυπτογράφησης ημερολογίου",
+	"diary-file": "Αρχείο Ημερολογίου",
+	"file-exists": "Ένα άλλο αρχείο υπάρχει στη τοποθεσία αυτή",
+	"move-error-msg": "Προέξυψε σφάλμα κατά τη μετακίνηση του αρχείου",
+	"move-error-title": "Σφάλμα μετακίνησης αρχείου",
+	"move-file": "Μετακίνηση αρχείου",
+	"new-password": "Νέος κωδικός",
+	"password": "Κωδικός",
+	"passwords-no-match": "Οι κωδικοί δεν ταιριάζουν",
+	"repeat-new-password": "Επανάληψη νέου κωδικού",
+	"repeat-password": "Επανάληψη κωδικού",
+	"select-directory": "Επιλογή τοποθεσίας",
+	"set-password": "Επιλογή κωδικού",
+	"unlock": "Ξεκλείδωμα",
+	"wrong-password": "Λάθος κωδικός",
+
+	// Import
+	"import-backup-info": "{appName} θα δημιουργήσει ένα αντίγραφο ασφαλείας των δεδομένων σας πρίν την εισαγωγή.",
+	"import-error-msg": "Προέκυψε ένα σφάλμα κατά την εισαγωγή",
+	"import-error-title": "Σφάλμα εισαγωγής",
+	"import-instructions-day-one": "Ανοίξτε την εφαρμογή Day One και εξάγετε το ημερολόγιο σας από το Αρχείο → Εξαγωγή → JSON. Αποσυμπιέστε το αρχείο που θα δημιουργηθεί. Επιλέξτε το αρχείο JSON που δημιουργήθηκε στο επόμενο βήμα για να το εισάγετε στο {appName}.",
+	"import-instructions-jrnl": "Για να εξάγετε το jrnl ημερολόγιο σας, τρέξτε την εντολή {command}. Επιλέξτε το αρχείο JSON που δημιουργήθηκε στο επόμενο βήμα για να το εισάγετε στο {appName}.",
+	"import-instructions-json": "Μπορείτε να εισάγετε τα δεδομένα σας από κάποια παλαιότερη εξαγωγή {appName} JSON ή από ένα άλλη JSON με την ίδια μορφή.",
+	"start-import": "Εκκίνηση εισαγωγής",
+
+	// Export
+	"export-error-msg": "Προέκυψε ένα σφάλμα κατά την εξαγωγή",
+	"export-error-title": "Σφάλμα εξαγωγής",
+
+	// Other
+	"invalid-date": "Μη έγκυρη ημερομηνία",
+	"loading": "Φορτώνει",
+};
+
+export default translationsEn;
