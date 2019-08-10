e27e292226a1558518ff3d0ffd96a48588487b54
Samuel Meuli <me@samuelmeuli.com>
2019-08-10 15:38:40 +0200

    Migrate to `strip-markdown` upstream

--- src/renderer/utils/mdToTxt.ts
+++ src/renderer/utils/mdToTxt.ts
@@ -8,11 +8,9 @@ import strip from "strip-markdown";
 export default async function mdToTxt(md: string): Promise<string> {
 	const txt = await remark()
 		// Remove Markdown formatting (except for list bullets/numbers)
-		.use(strip)
+		.use(strip, { keep: ["list", "listItem"] })
 		// Convert remaining Markdown to string
-		.use(stringify, {
-			listItemIndent: "1",
-		})
+		.use(stringify, { listItemIndent: "1" })
 		.process(md);
 	return txt.contents.toString();
 }
