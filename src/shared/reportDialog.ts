132557648cead58f20a3fb576573d7a81925a151
Samuel Meuli <me@samuelmeuli.com>
2019-12-08 17:30:22 +0100

    Improve default text for GitHub issues

--- src/shared/reportDialog.ts
+++ src/shared/reportDialog.ts
@@ -2,8 +2,34 @@ import unhandled from "electron-unhandled";
 import { debugInfo, openNewGitHubIssue } from "electron-util";
 
 /**
- * Catch unhandled errors and promise rejections. Display error dialog with report button. Reporting
- * will create a new GitHub issue with information about the error, app version and platform
+ * Returns the default text for a new GitHub issue (includes stack trace, app version, platform and
+ * error description)
+ */
+function getIssueText(stack: string | undefined): string {
+	return `**Stack trace:**
+
+\`\`\`
+${stack}
+\`\`\`
+
+---
+
+**Environment:**
+
+\`\`\`
+${debugInfo()}
+\`\`\`
+
+---
+
+**Error description:**
+
+<!-- Please provide a detailed description of what caused the error: What were you doing in the app, after what action did the error occur? -->`;
+}
+
+/**
+ * Catches unhandled errors and promise rejections. Displays an error dialog. Clicking the "Report"
+ * button opens a new GitHub issue with information about the error
  */
 export default function initReportDialog(): void {
 	unhandled({
@@ -11,7 +37,7 @@ export default function initReportDialog(): void {
 			openNewGitHubIssue({
 				user: "samuelmeuli",
 				repo: "mini-diary",
-				body: `\`\`\`\n${error.stack}\n\`\`\`\n\n---\n\n${debugInfo()}`,
+				body: getIssueText(error.stack),
 			});
 		},
 	});
