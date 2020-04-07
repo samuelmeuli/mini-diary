de0894caf824769a220325e666ecc32b41cbde2d
Samuel Meuli <me@samuelmeuli.com>
2020-04-07 14:31:18 +0200

    Use `fromIndexDate` function for parsing min/max dates

--- src/renderer/constants.ts
+++ src/renderer/constants.ts
@@ -1,6 +1,6 @@
-import { parseDate } from "./utils/dateFormat";
+import { fromIndexDate } from "./utils/dateFormat";
 
 // Dates
 
-export const MIN_DATE = parseDate("1900-01-01");
-export const MAX_DATE = parseDate("2099-12-31");
+export const MIN_DATE = fromIndexDate("1900-01-01");
+export const MAX_DATE = fromIndexDate("2099-12-31");
