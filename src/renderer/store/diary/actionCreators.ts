samuelmeuli/mini-diary

commit 3f7ffba4b570c435242197960fd7b8b90698f042

Handle all dates with Moment.js UTC mode
This should help prevent unwanted behavior due to implicit timezone conversions.

Fixes #69

src/renderer/store/diary/actionCreators.ts
@@ -1,6 +1,7 @@
-import moment from "moment";
+import moment, { Moment } from "moment";
 
 import { MAX_DATE, MIN_DATE } from "../../constants";
+import { createDate, parseDate } from "../../utils/dateFormat";
 import { searchIndex } from "../../utils/searchIndex";
 import { ThunkActionT } from "../store";
 import {
@@ -14,7 +15,7 @@ import {
 
 // Action creators
 
-export function setDateSelected(dateSelected: Date): SetDateSelectedAction {
+export function setDateSelected(dateSelected: Moment): SetDateSelectedAction {
 	let dateValidated = dateSelected;
 	if (dateSelected < MIN_DATE) {
 		dateValidated = MIN_DATE;
@@ -59,20 +60,20 @@ export const setDaySelectedNext = (): ThunkActionT => (dispatch, getState): void
 	const { app, diary } = getState();
 	const { allowFutureEntries } = app;
 	const { dateSelected } = diary;
-	const nextDay = moment(dateSelected).add(1, "days");
-	if (allowFutureEntries || nextDay.isSameOrBefore(moment(), "day")) {
-		dispatch(setDateSelected(nextDay.toDate()));
+	const nextDay = parseDate(dateSelected).add(1, "days");
+	if (allowFutureEntries || nextDay.isSameOrBefore(createDate(), "day")) {
+		dispatch(setDateSelected(nextDay));
 	}
 };
 
 export const setDaySelectedPrevious = (): ThunkActionT => (dispatch, getState): void => {
 	const { dateSelected } = getState().diary;
-	const previousDay = moment(dateSelected).subtract(1, "days");
-	dispatch(setDateSelected(previousDay.toDate()));
+	const previousDay = parseDate(dateSelected).subtract(1, "days");
+	dispatch(setDateSelected(previousDay));
 };
 
 export const setDaySelectedToday = (): ThunkActionT => (dispatch): void => {
-	const today = new Date();
+	const today = createDate();
 	dispatch(setDateSelected(today));
 };
 
@@ -80,18 +81,21 @@ export const setMonthSelectedNext = (): ThunkActionT => (dispatch, getState): vo
 	const { app, diary } = getState();
 	const { allowFutureEntries } = app;
 	const { monthSelected } = diary;
-	const nextMonth = moment(monthSelected)
+	const nextMonth = moment
+		.utc(monthSelected)
 		.add(1, "months")
 		.startOf("month");
-	if (allowFutureEntries || nextMonth.isSameOrBefore(moment(), "month")) {
-		dispatch(setDateSelected(nextMonth.toDate()));
+	const today = createDate();
+	if (allowFutureEntries || nextMonth.isSameOrBefore(today, "month")) {
+		dispatch(setDateSelected(createDate()));
 	}
 };
 
 export const setMonthSelectedPrevious = (): ThunkActionT => (dispatch, getState): void => {
 	const { monthSelected } = getState().diary;
-	const previousMonth = moment(monthSelected)
+	const previousMonth = moment
+		.utc(monthSelected)
 		.subtract(1, "months")
 		.startOf("month");
-	dispatch(setDateSelected(previousMonth.toDate()));
+	dispatch(setDateSelected(previousMonth));
 };

src/renderer/store/file/actionCreators.ts
@@ -12,7 +12,8 @@ import {
 	writeEncryptedFile,
 } from "../../files/fileAccess";
 import mergeEntries from "../../files/import/mergeEntries";
-import { Entries, MiniDiaryJson, IndexDate } from "../../types";
+import { Entries, IndexDate, MiniDiaryJson } from "../../types";
+import { createDate } from "../../utils/dateFormat";
 import { translations } from "../../utils/i18n";
 import { addIndexDoc, createIndex, removeIndexDoc, updateIndexDoc } from "../../utils/searchIndex";
 import { ThunkActionT } from "../store";
@@ -256,7 +257,7 @@ export const updateEntry = (entryDate: IndexDate, title: string, text: string): 
 	} else if (!(entryDate in entries)) {
 		// Entry doesn't exist yet
 		const entryAdded = {
-			dateUpdated: new Date().toString(),
+			dateUpdated: createDate().toString(),
 			title,
 			text,
 		};
@@ -269,7 +270,7 @@ export const updateEntry = (entryDate: IndexDate, title: string, text: string): 
 		// Entry has been updated
 		const entryOld = entries[entryDate];
 		const entryUpdated = {
-			dateUpdated: new Date().toString(),
+			dateUpdated: createDate().toString(),
 			title,
 			text,
 		};

src/renderer/components/elements/editor/editor-toolbar/word-count/WordCount.tsx
@@ -1,3 +1,4 @@
+import { Moment } from "moment";
 import React, { ReactElement } from "react";
 import countWords from "word-count";
 
@@ -5,7 +6,7 @@ import { Entries } from "../../../../../types";
 import { toIndexDate } from "../../../../../utils/dateFormat";
 
 export interface StateProps {
-	dateSelected: Date;
+	dateSelected: Moment;
 	entries: Entries;
 }
 

src/renderer/components/elements/editor/editor/Editor.tsx
@@ -14,6 +14,7 @@ import createListPlugin from "draft-js-list-plugin";
 import PluginEditor from "draft-js-plugins-editor";
 import debounce from "lodash.debounce";
 import { draftToMarkdown, markdownToDraft } from "markdown-draft-js";
+import { Moment } from "moment";
 import React, { KeyboardEvent, PureComponent, ReactNode } from "react";
 
 import { Entries, IndexDate } from "../../../../types";
@@ -30,7 +31,7 @@ const listPlugin = createListPlugin();
 const plugins = [listPlugin];
 
 export interface StateProps {
-	dateSelected: Date;
+	dateSelected: Moment;
 	entries: Entries;
 }
 
@@ -41,7 +42,7 @@ export interface DispatchProps {
 type Props = StateProps & DispatchProps;
 
 interface State {
-	dateSelected: Date;
+	dateSelected: Moment;
 	textEditorState: EditorState;
 	titleEditorState: EditorState;
 }
@@ -63,7 +64,7 @@ export default class Editor extends PureComponent<Props, State> {
 
 	static getStateFromEntry(
 		entries: Entries,
-		date: Date,
+		date: Moment,
 	): { textEditorState: EditorState; titleEditorState: EditorState } {
 		const indexDate = toIndexDate(date);
 		const entry = entries[indexDate];

src/renderer/components/elements/sidebar/calendar-nav/CalendarNav.tsx
@@ -1,16 +1,16 @@
 import PrevIcon from "feather-icons/dist/icons/chevron-left.svg";
 import NextIcon from "feather-icons/dist/icons/chevron-right.svg";
-import moment from "moment";
+import { Moment } from "moment";
 import React, { ReactElement } from "react";
 
 import { MAX_DATE, MIN_DATE } from "../../../../constants";
-import { toMonthYear } from "../../../../utils/dateFormat";
+import { createDate, parseDate, toMonthYear } from "../../../../utils/dateFormat";
 import { translations } from "../../../../utils/i18n";
 import { iconProps } from "../../../../utils/icons";
 
 export interface StateProps {
 	allowFutureEntries: boolean;
-	monthSelected: Date;
+	monthSelected: Moment;
 }
 
 export interface DispatchProps {
@@ -28,11 +28,11 @@ export default function CalendarNav(props: Props): ReactElement {
 		setMonthSelectedPrevious,
 	} = props;
 
-	const today = moment();
+	const today = createDate();
 
 	// Check if buttons for switching to previous/next month should be enabled. Determined based on
 	// the min/max dates and whether future diary entries are allowed
-	const month = moment(monthSelected);
+	const month = parseDate(monthSelected);
 	const canClickPrev = month.isAfter(MIN_DATE, "month");
 	const canClickNext =
 		month.isBefore(MAX_DATE, "month") && (allowFutureEntries || month.isBefore(today, "month"));

src/renderer/components/elements/sidebar/calendar/Calendar.tsx
@@ -1,23 +1,23 @@
-import moment from "moment";
+import { Moment } from "moment";
 import React, { PureComponent, ReactNode } from "react";
 import DayPicker from "react-day-picker";
 import MomentLocaleUtils from "react-day-picker/moment";
 
 import { Entries, Weekday } from "../../../../types";
-import { toIndexDate } from "../../../../utils/dateFormat";
+import { createDate, parseDate, toIndexDate } from "../../../../utils/dateFormat";
 import { lang } from "../../../../utils/i18n";
 import CalendarNavContainer from "../calendar-nav/CalendarNavContainer";
 
 export interface StateProps {
 	allowFutureEntries: boolean;
-	dateSelected: Date;
+	dateSelected: Moment;
 	entries: Entries;
 	firstDayOfWeek: Weekday | null;
-	monthSelected: Date;
+	monthSelected: Moment;
 }
 
 export interface DispatchProps {
-	setDateSelected: (date: Date) => void;
+	setDateSelected: (date: Moment) => void;
 }
 
 type Props = StateProps & DispatchProps;
@@ -32,28 +32,31 @@ export default class Calendar extends PureComponent<Props, {}> {
 
 	onDateSelection(date: Date): void {
 		const { allowFutureEntries, setDateSelected } = this.props;
+		const parsedDate = parseDate(date);
+		const today = createDate();
 
-		if (allowFutureEntries || moment(date).isSameOrBefore(moment(), "day")) {
-			setDateSelected(date);
+		if (allowFutureEntries || parseDate(date).isSameOrBefore(today, "day")) {
+			setDateSelected(parsedDate);
 		}
 	}
 
 	render(): ReactNode {
 		const { allowFutureEntries, dateSelected, entries, firstDayOfWeek, monthSelected } = this.props;
 
-		const today = new Date();
+		const today = createDate();
 		const daysWithEntries = Object.keys(entries);
-		const hasEntry = (day: Date): boolean => {
-			const indexDate = toIndexDate(day);
+
+		const hasEntry = (date: Date): boolean => {
+			const indexDate = toIndexDate(parseDate(date));
 			return daysWithEntries.includes(indexDate);
 		};
 
 		return (
 			<DayPicker
-				selectedDays={dateSelected}
-				disabledDays={allowFutureEntries ? null : { after: today }}
-				month={monthSelected}
-				toMonth={today}
+				selectedDays={dateSelected.toDate()}
+				disabledDays={allowFutureEntries ? null : { after: today.toDate() }}
+				month={monthSelected.toDate()}
+				toMonth={today.toDate()}
 				captionElement={(): null => null}
 				modifiers={{ hasEntry }}
 				firstDayOfWeek={firstDayOfWeek ?? undefined}

src/renderer/components/elements/sidebar/calendar/CalendarContainer.tsx
@@ -1,3 +1,4 @@
+import { Moment } from "moment";
 import { connect } from "react-redux";
 
 import { setDateSelected } from "../../../../store/diary/actionCreators";
@@ -14,7 +15,7 @@ const mapStateToProps = (state: RootState): StateProps => ({
 });
 
 const mapDispatchToProps = (dispatch: ThunkDispatchT): DispatchProps => ({
-	setDateSelected: (date: Date): SetDateSelectedAction => dispatch(setDateSelected(date)),
+	setDateSelected: (date: Moment): SetDateSelectedAction => dispatch(setDateSelected(date)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Calendar);

src/renderer/components/elements/sidebar/search-bar/SearchBar.tsx
@@ -1,21 +1,22 @@
 import ClearIcon from "feather-icons/dist/icons/x.svg";
 import debounce from "lodash.debounce";
-import moment from "moment";
+import { Moment } from "moment";
 import React, { ChangeEvent, PureComponent, ReactNode } from "react";
 
 import TodayIcon from "../../../../assets/icons/today.svg";
+import { createDate, parseDate } from "../../../../utils/dateFormat";
 import { translations } from "../../../../utils/i18n";
 import { iconProps } from "../../../../utils/icons";
 
 export interface StateProps {
-	dateSelected: Date;
-	monthSelected: Date;
+	dateSelected: Moment;
+	monthSelected: Moment;
 	searchKey: string;
 }
 
 export interface DispatchProps {
 	search: (searchKey: string) => void;
-	setDateSelected: (date: Date) => void;
+	setDateSelected: (date: Moment) => void;
 }
 
 type Props = StateProps & DispatchProps;
@@ -56,7 +57,7 @@ export default class SearchBar extends PureComponent<Props, State> {
 	onTodaySelection(): void {
 		const { setDateSelected } = this.props;
 
-		const today = new Date();
+		const today = createDate();
 		setDateSelected(today);
 	}
 
@@ -77,9 +78,9 @@ export default class SearchBar extends PureComponent<Props, State> {
 		const { dateSelected, monthSelected } = this.props;
 		const { newSearchKey } = this.state;
 
-		const today = moment();
-		const isToday = moment(dateSelected).isSame(today, "day");
-		const isCurrentMonth = moment(monthSelected).isSame(today, "month");
+		const today = createDate();
+		const isToday = parseDate(dateSelected).isSame(today, "day");
+		const isCurrentMonth = parseDate(monthSelected).isSame(today, "month");
 
 		return (
 			<div className="view-selector">

commit e954ba9f69d5dfe700d4883046bf826ed918646b

Fix behavior of `setMonthSelectedNext` function
A bug introduced in 3f7ffba caused the function to select the current day instead of the month after the selected day.

Fixes #73

src/renderer/store/diary/actionCreators.ts
@@ -1,4 +1,4 @@
-import moment, { Moment } from "moment";
+import { Moment } from "moment";
 
 import { MAX_DATE, MIN_DATE } from "../../constants";
 import { createDate, parseDate } from "../../utils/dateFormat";
@@ -61,7 +61,8 @@ export const setDaySelectedNext = (): ThunkActionT => (dispatch, getState): void
 	const { allowFutureEntries } = app;
 	const { dateSelected } = diary;
 	const nextDay = parseDate(dateSelected).add(1, "days");
-	if (allowFutureEntries || nextDay.isSameOrBefore(createDate(), "day")) {
+	const today = createDate();
+	if (allowFutureEntries || nextDay.isSameOrBefore(today, "day")) {
 		dispatch(setDateSelected(nextDay));
 	}
 };
@@ -81,21 +82,19 @@ export const setMonthSelectedNext = (): ThunkActionT => (dispatch, getState): vo
 	const { app, diary } = getState();
 	const { allowFutureEntries } = app;
 	const { monthSelected } = diary;
-	const nextMonth = moment
-		.utc(monthSelected)
-		.add(1, "months")
-		.startOf("month");
+	const nextMonth = parseDate(monthSelected)
+		.startOf("month")
+		.add(1, "months");
 	const today = createDate();
 	if (allowFutureEntries || nextMonth.isSameOrBefore(today, "month")) {
-		dispatch(setDateSelected(createDate()));
+		dispatch(setDateSelected(nextMonth));
 	}
 };
 
 export const setMonthSelectedPrevious = (): ThunkActionT => (dispatch, getState): void => {
 	const { monthSelected } = getState().diary;
-	const previousMonth = moment
-		.utc(monthSelected)
-		.subtract(1, "months")
-		.startOf("month");
+	const previousMonth = parseDate(monthSelected)
+		.startOf("month")
+		.subtract(1, "months");
 	dispatch(setDateSelected(previousMonth));
 };